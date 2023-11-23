64211e567db565895ff24b170ee6ed5bbb472485
Lumomag <lucasmoraesmagalhaes@gmail.com>
2023-11-23 09:35:15 -0300

    Adição da lógica do jogo
    Implementação do código que verifica a escolha do jogador e computador. Com base nisso, determina o vencedor!

--- script.js
+++ script.js
@@ -4,9 +4,22 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection) {
-  // your code here!
+
+  getComputerChoice()
+  
+  if (playerSelection === computerSelection){
+    return "It's a tie!"
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors' ||
+     playerSelection === 'paper' && computerSelection === 'rock' ||
+     playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    return `You won! ${playerSelection} beats ${computerSelection}!`
+  } else {
+    return `You lost! ${computerSelection} beats ${playerSelection}!`
+  }
 }
  
-const playerSelection = "rock";
+const playerSelection = prompt('Rock, Paper or Scissors?').toLocaleLowerCase();
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
