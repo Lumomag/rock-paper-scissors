2fe97d2d785ca82d9b5c12acb326c6851f700398
Lumomag <lucasmoraesmagalhaes@gmail.com>
2023-11-30 12:03:49 -0300

    Adição das funções updateGame e resetGame e o hoist das variáveis de score, para que sejam acessíveis às funções citadas

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * 3)];
@@ -17,59 +20,65 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-
 function game(playerSelection) {
-  let playerScore = 0;
-  let computerScore = 0;
 
   let player = playerSelection;
   let computer = getComputerChoice();
 
-
-  roundResult.innerHTML += (`<p>Player Selection: ${player}\n<\p>`);
-  roundResult.innerHTML += (`<p>Computer Selection: ${computer}</p>`);
+  roundResult.innerHTML += `<p>Player Selection: ${player}</p>`;
+  roundResult.innerHTML += `<p>Computer Selection: ${computer}</p>`;
 
   let result = playRound(player, computer);
-  roundResult.innerHTML += (result);
+  roundResult.innerHTML += result;
 
   if (result.includes("You won!")) {
     playerScore++;
   } else if (result.includes("You lost!")) {
     computerScore++;
   }
+  
+  updateScore();
+  
+  if (playerScore === 5) {
+    roundResult.innerHTML += "\n\tYou won the game!!!";
+    resetGame();
+  } else if (computerScore === 5) {
+    roundResult.innerHTML += "\tYou lost the game! ;-;";
+    resetGame();
+  } 
+}
 
-  roundResult.innerHTML += ("<p>---- Final result ----</p>");
-  roundResult.innerHTML += (`<p>Player Score: ${playerScore}</p>`);
-  roundResult.innerHTML += (`<p>Computer Score: ${computerScore}</p>`);
+function resetGame(){
+  playerScore = 0;
+  computerScore = 0;
+}
 
-  if (playerScore > computerScore) {
-    roundResult.innerHTML +=("\n\tYou won the game!!!");
-  } else if (playerScore < computerScore) {
-    roundResult.innerHTML +=("\tYou lost the game! ;-;");
-  } else {
-    roundResult.innerHTML +=("\tIt's a draw!");
-  }
-}  
+function updateScore(){
+  roundResult.innerHTML += "<p>---- Score (up to 5) ----</p>";
+  roundResult.innerHTML += `<p>Player Score: ${playerScore}</p>`;
+  roundResult.innerHTML += `<p>Computer Score: ${computerScore}</p>`;
 
-const rockButton = document.querySelector('.rock-btn');
-const paperButton = document.querySelector('.paper-btn');
-const scissorsButton = document.querySelector('.scissors-btn');
-const roundResult = document.querySelector('.result');
+}
 
-rockButton.addEventListener('click', () => {
-  roundResult.innerHTML = '';
-  game('rock')
- // roundResult.innerHTML = (playRound('rock', getComputerChoice()));
+const rockButton = document.querySelector(".rock-btn");
+const paperButton = document.querySelector(".paper-btn");
+const scissorsButton = document.querySelector(".scissors-btn");
+const roundResult = document.querySelector(".result");
+
+rockButton.addEventListener("click", () => {
+  roundResult.innerHTML = "";
+  game("rock");
+  // roundResult.innerHTML = (playRound('rock', getComputerChoice()));
 });
 
-paperButton.addEventListener('click', () => {
-  roundResult.innerHTML = '';
-  game('paper')
+paperButton.addEventListener("click", () => {
+  roundResult.innerHTML = "";
+  game("paper");
   //roundResult.innerHTML = (playRound('paper', getComputerChoice()));
-})
+});
 
-scissorsButton.addEventListener('click', () =>{
-  roundResult.innerHTML = '';
-  game('scissors')
+scissorsButton.addEventListener("click", () => {
+  roundResult.innerHTML = "";
+  game("scissors");
   //roundResult.innerHTML = (playRound('scissors', getComputerChoice()));
-})
+});
