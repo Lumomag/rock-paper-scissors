7182e4698cb7b22c25c16701903b560442f22dae
Lumomag <lucasmoraesmagalhaes@gmail.com>
2023-11-30 11:26:15 -0300

    Implementação de botões e respectiva lógica para selecionar opção e mostrar resultado

--- script.js
+++ script.js
@@ -1,58 +1,70 @@
-
-
-
-function getComputerChoice(){
-  const choices = ['rock', 'paper', 'scissors']
-  return choices[Math.floor(Math.random() * 3)]
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection){
-    return "\n\t\tIt's a tie!"
+  if (playerSelection === computerSelection) {
+    return "\n\t\tIt's a tie!";
   } else if (
-    (playerSelection === 'rock' && computerSelection === 'scissors') ||
-     (playerSelection === 'paper' && computerSelection === 'rock') ||
-     (playerSelection === 'scissors' && computerSelection === 'paper')
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return `\n\t\tYou won! ${playerSelection} beats ${computerSelection}!`
+    return `\n\t\tYou won! ${playerSelection} beats ${computerSelection}!`;
   } else {
-    return `\n\t\tYou lost! ${computerSelection} beats ${playerSelection}!`
+    return `\n\t\tYou lost! ${computerSelection} beats ${playerSelection}!`;
   }
 }
 
-function game(){
-  let playerScore = 0
-  let computerScore = 0
+/*
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
 
-  for (let round = 1; round <= 5; round++){
-    let player = prompt('Rock, Paper or Scissors?')
-    let computer = getComputerChoice();
+  let player = '';
+  let computer = getComputerChoice();
 
-    console.log(`\nRound: ${round}`);
-    console.log(`\t\tPlayer Selection: ${player}`);
-    console.log(`\t\tComputer Selection: ${computer}`);
 
-    let result = playRound(player, computer)
-    console.log(result)
+  console.log(`\t\tPlayer Selection: ${player}`);
+  console.log(`\t\tComputer Selection: ${computer}`);
 
-    if (result.includes('You won!')){
-      playerScore++
-    } else if (result.includes('You lost!')){
-      computerScore++
-    }
+  let result = playRound(player, computer);
+  console.log(result);
+
+  if (result.includes("You won!")) {
+    playerScore++;
+  } else if (result.includes("You lost!")) {
+    computerScore++;
   }
 
-  console.log('\n---- Final result ----\n')
+  /*
+  console.log("\n---- Final result ----\n");
   console.log(`\tPlayer Score: ${playerScore}`);
   console.log(`\tComputer Score: ${computerScore}\n`);
 
-  if (playerScore > computerScore){
-    console.log('\tYou won the game!!!')
-  } else if (playerScore < computerScore){
-    console.log('\tYou lost the game! ;-;')
+  if (playerScore > computerScore) {
+    console.log("\tYou won the game!!!");
+  } else if (playerScore < computerScore) {
+    console.log("\tYou lost the game! ;-;");
   } else {
-    console.log("\tIt's a draw!")
+    console.log("\tIt's a draw!");
   }
-}
+  
+*/
 
-game()
+const rockButton = document.querySelector('.rock-btn');
+const paperButton = document.querySelector('.paper-btn');
+const scissorsButton = document.querySelector('.scissors-btn');
+
+rockButton.addEventListener('click', () => {
+  console.log(playRound('rock', getComputerChoice()));
+});
+
+paperButton.addEventListener('click', () => {
+  console.log(playRound('paper', getComputerChoice()));
+})
+
+scissorsButton.addEventListener('click', () =>{
+  console.log(playRound('scissors', getComputerChoice()));
+})
